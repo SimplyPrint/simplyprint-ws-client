c6a20209a504856ee92658cdb9faa9b22c2f04e8
Rumrobot <46647057+Rumrobot@users.noreply.github.com>
2026-07-01 13:54:12 +0200

    fix: removed Elegoo Canvas from can_chain

--- simplyprint_ws_client/core/state/models.py
+++ simplyprint_ws_client/core/state/models.py
@@ -203,7 +203,6 @@ class MultiMaterialSolution(Enum):
         return self in {
             self.CREALITY_CFS,
             self.ANYCUBIC_ACE_PRO,
-            self.ELEGOO_CANVAS,
             self.BAMBU_AMS,
             self.BAMBU_AMS_2_PRO,
             self.BAMBU_AMS_HT,
@@ -219,7 +218,6 @@ class MultiMaterialSolution(Enum):
             self.BAMBU_AMS_LITE: 1,
             self.CREALITY_CFS: 4,
             self.ANYCUBIC_ACE_PRO: 4,
-            self.ELEGOO_CANVAS: 4,
         }.get(self)
 
     @property
